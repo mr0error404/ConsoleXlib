f7b77129c317e172bac1f41b3ce0981080a19820
Abdullah Yasein <101466323+mr0error404@users.noreply.github.com>
2024-04-20 21:32:40 +0300

    Sec

--- consoleXLib/Manager.cs
+++ consoleXLib/Manager.cs
@@ -3,9 +3,43 @@ namespace consoleXLib
 {
 	public class Manager
 	{
-		public Manager()
-		{
-		}
-	}
+        // Properties
+        private int ManagerId { get; set; }
+        private string Username { get; set; }
+        private int RoleId { get; set; }
+        private string Password { get; set; }
+        private string Mobile { get; set; }
+        private string Email { get; set; }
+
+        // Methods
+        public void AddUser(){  }
+
+        public void EditUser(){ }
+
+        public void DeleteUser() { }
+
+        public void SearchUser() { }
+
+        public void SearchBook() { }
+
+        public void SearchCategory() { }
+
+        public void AddCategory() { }
+
+        public void EditCategory() { }
+
+        public void DeleteCategory() { }
+
+        public void SearchSubCategory() { }
+
+        public void AddSubCategory() { }
+
+        public void EditSubCategory() { }
+
+        public void DeleteSubCategory() { }
+
+        public void UpdateBookStatus() { }
+
+    }
 }
 
