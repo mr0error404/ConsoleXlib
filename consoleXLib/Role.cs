1245df0d21c7aea2eb8bc3fdc034ef46239406b5
Abdullah Yasein <101466323+mr0error404@users.noreply.github.com>
2024-05-04 14:40:33 +0300

    YNS —> Done !!!!

---
+++ consoleXLib/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace consoleXLib
+{
+	public class Role
+	{
+		public Role()
+		{
+		}
+	}
+}
+
---
+++ consoleXLib/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace consoleXLib
+{
+	public class User
+	{
+		public User()
+		{
+		}
+	}
+}
+
