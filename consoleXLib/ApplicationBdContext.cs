ad3d76a5a898385b1b3373d657ab79555bf804eb
Abdullah Yasein <101466323+mr0error404@users.noreply.github.com>
2024-05-04 21:22:44 +0300

    Add Est —> done

--- consoleXLib/ApplicationBdContext.cs
+++ consoleXLib/ApplicationBdContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace consoleXLib
 {
-	public class ApplicationBdContext:DbContext
-	{
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-           optionsBuilder.UseSqlServer("Data Source=DESKTOP-LDMN0NB\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Trust Server Certificate=True");
-	}
+    public class ApplicationBdContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=consoleXLib.db");
+        }
+    }
 }
-
