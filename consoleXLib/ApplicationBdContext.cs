fcc1b443f2a0be224a1080e7ab383c65969a3536
Abdullah Yasein <101466323+mr0error404@users.noreply.github.com>
2024-05-04 21:23:44 +0300

    Done —> done

--- consoleXLib/ApplicationBdContext.cs
+++ consoleXLib/ApplicationBdContext.cs
@@ -4,10 +4,22 @@ using Microsoft.EntityFrameworkCore;
 namespace consoleXLib
 {
     public class ApplicationBdContext : DbContext
-    {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite("Data Source=consoleXLib.db");
-        }
+	{
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+           optionsBuilder.UseSqlServer("Data Source=DESKTOP-LDMN0NB\\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;Trust Server Certificate=True");
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookState> BookStates { get; set; }
+        public DbSet<Laibrarian> Laibrarians { get; set; }
+        public DbSet<MainCategory> MainCategorys { get; set; } 
+        public DbSet<Manager> Manager { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<SubCategory> SubCategorys { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
     }
 }
+
