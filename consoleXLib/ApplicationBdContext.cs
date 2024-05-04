bfff2d0c1a38f6b9077798ebd50b8f894854a6a6
Abdullah Yasein <101466323+mr0error404@users.noreply.github.com>
2024-05-04 16:50:54 +0300

    addPackages —> Done

---
+++ consoleXLib/ApplicationBdContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace consoleXLib
+{
+	public class ApplicationBdContext:DbContext
+	{
+		public ApplicationBdContext()
+		{
+
+		}
+	}
+}
+
